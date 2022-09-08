0afa184abd55999d90a052424628c1e53405b8a1
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-08 18:12:06 -0300

    fix: lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,4 +2,4 @@
 // proteínas maior ou igual a 30 e menor ou igual a 40
 db.produtos.find({
   "valoresNutricionais.3.percentual": { $gte: 30, $lte: 40 },
-}, { _id: 0, nome: 1 })
+}, { _id: 0, nome: 1 });
