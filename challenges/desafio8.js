2876cfaf53309e23b9789a594831647e7dd82bfd
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-08 17:43:14 -0300

    feat: requisito 08 Deleta os lanches com menos de 50 curtidas e retorne o nome dos lanches que restaram no banco

---
+++ challenges/desafio8.js
@@ -0,0 +1,5 @@
+// Delete os lanches com menos de 50 curtidas e retorne o nome dos lanches que restaram no banco
+db.produtos.deleteMany({
+  curtidas: { $lte: 50 },
+});
+db.produtos.find({}, { _id: 0, nome: 1 });
