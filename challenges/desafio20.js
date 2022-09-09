9d1be587fa1e7f103cd31ba8964ee14968b094f5
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-09 08:34:25 -0300

    feat: requisito 20 remove o primeiro ingrediente do sanduíche Quarteirão com Queijo

---
+++ challenges/desafio20.js
@@ -0,0 +1,5 @@
+// Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo
+db.produtos.updateMany({ nome: "Quarteirão com Queijo" }, {
+    $pop: { ingredientes: -1 },
+});
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
