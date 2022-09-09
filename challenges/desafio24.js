e2c5b0b38e1883ecccf23fcee2a4b61f09d4e85d
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-09 15:28:24 -0300

    feat: requisito 24 ordena em todos os documentos os valores do array valoresNutricionais pelo campo percentual de forma decrescente

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,7 +1,6 @@
 // Ordene em todos os documentos os valores do array valoresNutricionais
 // pelo campo percentual de forma decrescente
-// db.produtos.updateMany({}, {
-//     $set: {"valoresNutricionais.$[element].percentual"}
-//     // $push: { "valoresNutricionais.percentual": { $each: [], $sort: -1 } },
-// });
-// db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
+db.produtos.updateMany({}, {
+  $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } },
+});
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
